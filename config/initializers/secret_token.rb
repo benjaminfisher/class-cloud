0438651d8b729d3393e75b1c114cdf88b6c5944f
Nate Goldman <nnmgoldman@gmail.com>
2011-11-09 20:23:19 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassCloud::Application.config.secret_token = 'b0fb6f084205638720fd7347f4847603967473b4bdeff8c74539fe03213bd56be18c10ceb8a48806dbfe525103b96e0b43c647d26e8261c41a6f169ab0973c1b'
